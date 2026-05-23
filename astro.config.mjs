615f0b2ebac3208436ba832a93ef68c93b0a86e4
Camila Lucena <camilalucena.cali@gmail.com>
2026-05-23 16:33:53 -0300

    fix: atualiza URL para cali-site.vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://spinelab.vercel.app',
+  site: 'https://cali-site.vercel.app',
   output: 'static',
 });
